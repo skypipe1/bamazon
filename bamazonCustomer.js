ef61d06b3e0a4b4349f391287c4dfc418d089250
Piper <skylarpiper710@gmail.com>
2019-02-27 13:37:58 -0600

    got reccomended finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,105 +3,146 @@ var mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
+    host: 'localhost',
+    port: 3306,
 
-	user: 'root',
+    user: 'root',
 
-	password: 'Minecraft420',
-	database: 'Bamazon'
+    password: 'Minecraft420',
+    database: 'Bamazon'
 });
 
-// welcomes user to Bamazon
-function welcomePrompt(){
+function welcomePrompt() {
     inquirer.prompt([{
         type: 'confirm',
         name: "confirm",
-        message: 'Welcome to Bamazon! would you like to shop with us today?',
+        message: 'Welcome to Bamazon! Would you like to shop with us today?',
         default: true
-    }]).then(function(input){
-        if (input.confirm === true){
+    }]).then(function (input) {
+        if (input.confirm === true) {
             promptPurchase();
-        }else{
-            console.log("Thank you, please visit again soon!")
+        } else {
+            console.log("Thank you come again!")
         }
     })
 }
 
-function promptPurchase(){
+function promptPurchase() {
     inquirer.prompt([{
         type: 'input',
         name: 'item_id',
         message: 'Please enter the Item ID which you would like to purchase.',
-        validate: validateInput,
+        validate: checkForNumberInput,
         filter: Number
     },
     {
         type: 'input',
         name: 'quantity',
-        message: 'How many do you want?',
-        validate: validateInput,
+        message: 'How many do you need?',
+        validate: checkForNumberInput,
         filter: Number
-    }])
+    }]).then(function (input) {
 
+        var item = input.item_id;
+        var quantity = input.quantity;
+
+        var queryStr = 'SELECT * FROM products WHERE ?';
+
+        connection.query(queryStr, { item_id: item }, function (err, data) {
+            if (err) throw err;
+
+            // If the user has selected an invalid item ID, data attay will be empty
+            // console.log('data = ' + JSON.stringify(data));
+
+            if (data.length === 0) {
+                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                displayInventory();
+
+            } else {
+                var productData = data[0];
+
+                // If the quantity requested by the user is in stock
+                if (quantity <= productData.stock_quantity) {
+                    console.log('The product you requested is in stock! Placing order!');
+
+                    // updating query string
+                    var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+                    
+
+                    // Update inventory
+                    connection.query(updateQueryStr, function (err, data) {
+                        if (err) throw err;
+
+                        console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+                        console.log('Thank you for shopping with us!');
+                        console.log("\n---------------------------------------------------------------------\n");
+
+                        
+                        connection.end();
+                    })
+                } else {
+                    console.log('There is not enough product in stock, your order can not be placed.');
+                    console.log('Please modify your order.');
+                    console.log("\n---------------------------------------------------------------------\n");
+
+                    displayInventory();
+                }
+            }
+        })
+    })
 }
 
 
-
-// function will only allow the user to input a positive number
-function validateInput(value) {
-	var integer = Number.isInteger(parseFloat(value));
-	var sign = Math.sign(value);
-
-	if (integer && (sign === 1)) {
-		return true;
-	} else {
-		return 'Please enter a whole non-zero number.';
-	}
-}
-// welcomePrompt();
-
-
-// displayInventory displays inventory, then calls welcomePrompt function to welcome the user.
+// displayInventory will retrieve the current inventory from the database and output it to the console
 function displayInventory() {
-	
 
-	
-	queryProducts = 'SELECT * FROM products';
 
-	// Make the db query
-	connection.query(queryProducts, function(err, data) {
-		if (err) throw err;
+    
+    queryProducts = 'SELECT * FROM products';
 
-		console.log('Existing Inventory: ');
-		console.log('...................');
+   
+    connection.query(queryProducts, function (err, data) {
+        if (err) throw err;
 
-		var itemTable = '';
-		for (var i = 0; i < data.length; i++) {
-			itemTable = '';
-			itemTable += 'Item ID: ' + data[i].item_id + '  ||  ';
-			itemTable += 'Product Name: ' + data[i].product_name + '  ||  ';
-			itemTable += 'Department: ' + data[i].department_name + '  ||  ';
-			itemTable += 'Price: $' + data[i].price;
-            
-			console.log(itemTable);
-		}
+        console.log('Existing Inventory: ');
+        console.log('...................\n');
 
-	  	console.log("---------------------------------------------------------------------");
+        var itemTable = '';
+        for (var i = 0; i < data.length; i++) {
+            itemTable = '';
+            itemTable += 'Item ID: ' + data[i].item_id + '  ||  ';
+            itemTable += 'Product Name: ' + data[i].product_name + '  ||  ';
+            itemTable += 'Department: ' + data[i].department_name + '  ||  ';
+            itemTable += 'Price: $' + data[i].price + '\n';
 
-	  	
-          // promptPurchase();
+            console.log(itemTable);
+        }
+
+        console.log("---------------------------------------------------------------------\n");
+
+
         welcomePrompt();
-	})
+    })
 }
 
 
 
 function runBamazonApp() {
-
-
+    // Display the available inventory
     displayInventory();
 }
 
-// Run the application
+// starts the application
 runBamazonApp();
+
+// function will only allow the user to input a positive number
+function checkForNumberInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a positive number!.';
+    }
+}
