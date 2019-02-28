75ebda32e452a669e35d07df8bb452b413b0bdcb
Piper <skylarpiper710@gmail.com>
2019-02-28 17:51:18 -0600

    had to swap contents of sql files,

--- bamazon.sql
+++ bamazon.sql
@@ -9,3 +9,23 @@ CREATE TABLE products (
 	stock_quantity INTEGER(11) NOT NULL,
 	PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Computer Keyboard', 'Electronics', 32.47, 450),
+		('Gillette Mach3 Mens Razor w/ 15 refills', 'Cosmetics', 20.26, 500),
+		('Dash Cam', 'Electronics', 79.99, 350),
+		('Air Fryer', 'Home & Kitchen', 31.99, 800),
+		('Green Toys Rescue Boat with Helicopter', 'Toys & Games', 18.21, 200),
+		('VetWell Dog Ear Wipe','Pet Supplies', 9.99, 267),
+		('Featherd Pillow ', 'Home & Kitchen', 31.51, 200),
+		('EASELAND Soft Quilted Down  Comforter', 'Home & Kitchen', 47.92, 270),
+		('Joopin Semi Rimless Polarized Sunglasses', 'Clothing', 12.99, 575),
+		('Camping Chair', 'Sports & Outdoor', 21.50, 420),
+        ('DreamSky Alarm Clock Radio', 'Electronics', 21.24, 210),
+		('ARTEZA 9X12 Sketch Book', 'Arts & Crafts', 14.99, 400),
+		('TaoTronics Noise Cancelling Bluetooth Headphones', 'Electronics', 49.99, 150),
+		('Pop Up Tent', 'Sports & Outdoors', 32.99, 120),
+		('Stark Safe Cut Resistant Gloves', 'Tools & Home Improvement', 17.88, 160),
+		('Home Security Camera', 'Electronics', 59.49, 210),
+		('Heat Resistant Kitchen Oven Mitts', 'Home & Kitchen', 12.99, 180),
+		('EXPO Low Odor Dry Erase Marker 12PK', 'Arts & Crafts', 21.99, 400)
